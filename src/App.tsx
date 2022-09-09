737c13ad54012e82976feb55aad2f6233025afea
VanessaMaia26 <vanessamaia2607@gmail.com>
2022-09-09 14:36:43 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClick = () => {
+    setShow( !show );
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>{show ? 'Ocultar' : 'Mostrar'}</button>
+
+      {show &&
+      <div>
+        Um texto qualquer...
+      </div>
+      }
+    </div>
+  )
+}
+
+export default App;
